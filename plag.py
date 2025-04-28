77b85999c55b6c9b42dc1dbe2a6a947db2fa7420
Jayaraj U B <ubelamagi@gmail.com>
2025-04-28 16:30:42 +0530

    Update plag.py

--- plag.py
+++ plag.py
@@ -30,5 +30,5 @@ def check_plagiarism():
 
 if __name__ == "__main__":
      port = int(os.environ.get("PORT", 10000))  # Get the port from the environment or default to 10000
-    app.run(debug=True, host="0.0.0.0", port=port)
+     app.run(debug=True, host="0.0.0.0", port=port)
 
