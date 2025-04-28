73d5d6943cb29cda27dba74c3d48bfa96c5f7fae
Jayaraj U B <ubelamagi@gmail.com>
2025-04-28 16:43:45 +0530

    Update plag.py

--- plag.py
+++ plag.py
@@ -29,6 +29,6 @@ def check_plagiarism():
     return render_template("plqaga.html", question=question)
 
 if __name__ == "__main__":
-     port = int(os.environ.get("PORT", 10000))  # Get the port from the environment or default to 10000
+     port = int(os.environ.get("PORT", 5000))  # Get the port from the environment or default to 10000
      app.run(debug=True, host="0.0.0.0", port=port)
 
